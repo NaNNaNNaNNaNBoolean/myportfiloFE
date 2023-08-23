ad9ab9c58cac8a3f39055af02a1e850b543dee2a
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-08-23 14:55:13 +0100

    added pfp

--- src/components/AboutInfo/index.jsx
+++ src/components/AboutInfo/index.jsx
@@ -7,8 +7,8 @@ const AboutInfo = () => {
                     <p>Hello, I'm Nicole. I have a MEng in Electronic Engineering and a love of programming!</p>
                 </div>
                 <div className='me'>
-                    image
-                    <img src=''></img>
+                    {/* image
+                    <img src=''></img> */}
                 </div>
                 <div className='fade-about-info-items-2' style={{ animationDelay: `1500ms` }}>
                 <p>Coding Langugues I know are C++, C#, JavaScript and Python. I'm always eager to learn more and improve!</p>
