1fc976a3c6f4b7f06badc29ce4668bb35dc4c73f
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-08-30 16:07:28 +0100

    added contact me section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -21,7 +21,13 @@ const Home = () => {
       <div className='item3'>
         <button onClick={handleClick} className='goToProjects'><span>See Projects</span></button>
         {/* <NavBar /> */}
+        <h3 className='contactme'>Contact Me</h3>
+        <div className='connectwithme'> 
+          <button onClick={(e) => {e.preventDefault(); window.location.href='https://www.linkedin.com/in/nicole-french-a52840173/';}}>LinkedIn</button>
+          <button onClick={(e) => {e.preventDefault(); window.location.href='https://github.com/NaNNaNNaNNaNBoolean';}}>GitHub</button>
+        </div>
       </div>
+      
     </div>
     
     
