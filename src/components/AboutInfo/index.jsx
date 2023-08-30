845d369820a2b63a29ff5b85f96e917f789eac9f
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-08-30 15:51:55 +0100

    added bold to the hello

--- src/components/AboutInfo/index.jsx
+++ src/components/AboutInfo/index.jsx
@@ -11,8 +11,8 @@ const AboutInfo = () => {
                 <div className='fade-about-info-items-2' style={{ animationDelay: `1500ms` }}>
                 <div className='paragintro'>
                     <p>Coding Langugues I know are C++, C#, JavaScript and Python. I'm always eager to learn more and improve!</p>
-                    <p>*Please note this website is still a WIP many features are still to be added :) *</p>
-                    <p>*Also please be patience, the API is run on a free render host so takes a few minutes to start up*</p>
+                    <p><b>*Please note this website is still a WIP many features are still to be added :) *</b></p>
+                    <p><b>*Also please be patience, the API is run on a free render host so takes a few minutes to start up*</b></p>
                 </div>
                 </div>
             </div>
