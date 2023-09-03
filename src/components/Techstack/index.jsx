cd990e0dc6ca141f0b868640c0abc8679920c745
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-09-03 21:48:06 +0100

    improved tech stack

---
+++ src/components/Techstack/index.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import Boot from '../../imgs/code/Bootstrap.png'
+import Css from '../../imgs/code/css.png'
+import Dnd from '../../imgs/code/dnd.png'
+import Express from '../../imgs/code/express.png'
+import Figma from '../../imgs/code/figma.png'
+import Flask from '../../imgs/code/flask.png'
+import Github from '../../imgs/code/Github-Logo.png'
+import Html from '../../imgs/code/html.png'
+import Jest from '../../imgs/code/jest.png'
+import MongoDB from '../../imgs/code/MongoDB_Logo.png'
+import Moogoose from '../../imgs/code/moogoose.png'
+import Node from '../../imgs/code/node.png'
+import NotNode from '../../imgs/code/noode.png'
+import Py from '../../imgs/code/py.png'
+import Postgres from '../../imgs/code/postgresql_elephant.png'
+import Pytest from '../../imgs/code/Pytest.png'
+import reactImg from '../../imgs/code/react.png'
+import Render from '../../imgs/code/render.png'
+import vitest from '../../imgs/code/vitest.png'
+import Javascript from '../../imgs/code/JavaScript.png'
+import Cplusplus from '../../imgs/code/C++.png'
+import Csharp from '../../imgs/code/c-sharp.png'
+import Gdscript from '../../imgs/code/gdscript.png'
+
+const Techstack = () => {
+  return (
+    <>
+        <h2> Tech stack</h2>
+        
+        <h3> Langugues </h3>
+        <img src = {Cplusplus} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {Csharp} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {Javascript} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {Gdscript} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {Py} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+       
+        <h3> Backend </h3>
+        <img src = {Node} style = {{height: '2.5em', width: '3em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {Express} style = {{height: '2.5em', width: '3em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {Flask} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%' , marginLeft: '0.2em'}}></img>
+        <img src = {Postgres} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {MongoDB} style = {{height: '2.5em', width: '3.1em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {Moogoose} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {NotNode} style = {{height: '2.5em', width: '3.2em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {Jest} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {Pytest} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%', marginLeft: '0.2em'}}></img>
+        
+        <h3> Frontend </h3>
+        <img src = {Html} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {Css} style = {{height: '2.5em', width: '2em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {reactImg} style = {{height: '2.5em', width: '2.6em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {Boot} style = {{height: '2.5em', width: '2.6em', backgroundSize: '100% 100%', marginLeft: '0.2em'}}></img>
+        <img src = {Dnd} style = {{height: '2.5em', width: '2.7em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        <img src = {vitest} style = {{height: '2.5em', width: '2.5em', backgroundSize: '100% 100%', marginLeft: '0.2em' }}></img>
+        
+        <h3>Design/Deployment</h3>
+        <img src = {Figma} style = {{height: '2.5em', width: '4em', backgroundSize: '100% 100%' }}></img>
+        <img src = {Render} style = {{height: '2.5em', width: '4em', backgroundSize: '100% 100%' }}></img>
+        <img src = {Github} style = {{height: '2.5em', width: '4em', backgroundSize: '100% 100%' }}></img>
+    </>
+    
+  )
+}
+
+export default Techstack
