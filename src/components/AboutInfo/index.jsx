73870bba75f140269997947241843c684803ab98
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-09-09 20:47:48 +0100

    a lil fix to project list links

--- src/components/AboutInfo/index.jsx
+++ src/components/AboutInfo/index.jsx
@@ -10,8 +10,7 @@ const AboutInfo = () => {
                     <p>I have a MEng in Electronic Engineering and a passion for programming! I love trying out random ideas and experimenting with my code and I'm all about continuous improvement. I'm currently teaching myself GoLang. </p>
                     <p>Apart from coding, I love to draw, binge movies, play boardgames and as well as playing video games, I'm also developing my own! </p>
                     <p>I will be slowly adding my projects here as I develop this page further.</p>
-                    {/* <p>Coding Langugues I know are C++, C#, JavaScript and Python. I'm always eager to learn more and improve!</p> */}
-                    <p><b>Notice: </b> <u>Development on this  has been paused for this current week due to me being on hoilday :)</u> </p>
+                    {/* <p><b>Notice: </b> <u>Development on this  has been paused for this current week due to me being on hoilday :)</u> </p> */}
                     <p><b>*Please note this website is still a WIP many features/versions are still to be added :) Currently only suitable for desktops/laptops*</b></p>
                 </div>
                 <div className='me'>
