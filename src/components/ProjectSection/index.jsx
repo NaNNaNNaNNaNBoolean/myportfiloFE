18d0a135375eb3e75a099e289ad945022e4aa3ab
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-09-16 02:09:55 +0100

    adding some ani

--- src/components/ProjectSection/index.jsx
+++ src/components/ProjectSection/index.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+// require('intersection-observer');
+import handleViewport from 'react-in-viewport';
 import './style.css'
+import './animation.css'
 
-const ProjectSection = () => {
-
+const ProjectSection = (props) => {
+    const [aniStyle, setAniStyle] = useState(false)
+    const [vis,setVis] = useState(false)
+    const { inViewport, forwardedRef, enterCount } = props;
     const handleClick = () => {
         nav('/Projects')
     }
+    const myStyle = {animationPlayState: aniStyle ? 'running' : '', display: aniStyle ? 'block' : 'none'}
+    useEffect(()=>{
+         if(inViewport){
+         setAniStyle(true)
+        //  setVis(true)
+        }else{setAniStyle(false)}
+    })
+   
+   
 
   return (
     <div>
-       <div className='ProjectView'>
-            <div className='projecthighlighteditem' id='bouncein'></div>
+       <div className="viewport-block" ref={forwardedRef}>
+            <div className='projecthighlighteditem' id='bounceinbox' style ={myStyle}></div>
             <div className='projecthighlightedinfo'></div>
        </div>
-       <button  onClick={handleClick} className='goToProjects'  ><span className='glowing-txt'>SEE PROJECTS</span></button> 
+       <button  onClick={handleClick} className='goToProjects' ><span className='glowing-txt'>SEE PROJECTS</span></button> 
         <h2>section is under development</h2>
     
     </div>
   )
 }
+  const ViewportBlock = handleViewport(ProjectSection, /** options: {}, config: {} **/);
 
-export default ProjectSection
+    
+  const Component = (props) => (
+    <div>
+        <div style={{ height: '20vh' }}>
+            {/* <h2>Scroll down to make component in viewport</h2> */}
+        </div>
+        <ViewportBlock onEnterViewport={() => console.log('enter')} onLeaveViewport={() => console.log('leave')} />
+    </div>
+)
+
+
+export default Component
