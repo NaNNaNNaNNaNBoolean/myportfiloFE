0ecc2f1dc0dc5ea0c2633ee04406c9787d0ea755
N French :) <72806068+NaNNaNNaNNaNBoolean@users.noreply.github.com>
2023-09-21 13:06:04 +0100

    robot arm added to homepage

--- src/components/ProjectSection/index.jsx
+++ src/components/ProjectSection/index.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import handleViewport from 'react-in-viewport';
 import './style.css'
 import './animation.css'
+import './arm.css'
 import { useNavigate } from 'react-router-dom';
 
 const ProjectSection = (props) => {
@@ -13,6 +14,9 @@ const ProjectSection = (props) => {
     const nav = useNavigate()
     const handleClicksec = () => {
         nav('/Projects')
+    }
+    const handleClickDisplay =() => {
+      nav('/projects/9')
     }
     const myStyle = {animationPlayState: aniStyle ? 'running' : '', display: aniStyle ? 'block' : 'none'}
     useEffect(()=>{
@@ -40,15 +44,27 @@ const ProjectSection = (props) => {
     <div>
        <div className="viewport-block" ref={forwardedRef}>
             <div></div>
-            <div className='projecthighlighteditem' id='bounceinbox' style ={myStyle}> <h2>placeholder</h2>
+            <div className='dropdownani' >
+              <div className='arm-container'style={myStyle} >
+                <div className='armpart'></div>
+                <div className='hand'>
+                  <div className='handback' style={myStyle}></div>
+                  <div className='handfront' style={myStyle}></div>
+                </div>
+                
+              </div>
+              <div className='projecthighlighteditem' id='bounceinbox' style ={myStyle}> <h2>placeholder</h2>
+              <button onClick={handleClickDisplay} className='goToProjects2' >see more about this project</button>
+              </div>
             </div>
+         
             <div className='glitchcontainer'>
               <div className='projecthighlightedinfo' id = 'glitchin' style ={myStyle}>
                 <h2>{projectSec.name}</h2>
                 <h3>{ projectSec.subhead}</h3>
                 <video controls src={projectSec.video} type="video/mp4" style={{display: 'flex',width: '100%', padding:'1vh'}}></video>
               </div>
-             
+              
             </div>
            
             <div></div>
